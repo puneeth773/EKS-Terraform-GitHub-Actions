526caf9e8a335e34f04c804f755938f45b87144d
puneeth773 <70529791+puneeth773@users.noreply.github.com>
2026-09-03 15:52:12 +0530

    Update eks.tf

--- module/eks.tf
+++ module/eks.tf
@@ -14,7 +14,7 @@ resource "aws_eks_cluster" "eks" {
 
 
   access_config {
-    authentication_mode                         = "CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
