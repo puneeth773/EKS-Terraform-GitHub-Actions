a9639162e7f5dce3d0abfae4f00854e75be19ade
puneeth773 <70529791+puneeth773@users.noreply.github.com>
2026-09-24 14:13:55 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-reddyrocks-tf-bucket-us-east-1"
+    bucket         = "devops-platform-reddy-terraform-state-629068384298 "
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
